938cbe53eebb0e6e0e2bd6dbeb2b655fd09ab4e6
IURI <123806396+ithauront@users.noreply.github.com>
2023-04-25 20:06:39 +0200

    import coffeecups
    imported all the coffeecups images and put them in assets/coffeecup
    also started to do the styles for coffeelist

--- src/components/coffeelist/index.tsx
+++ src/components/coffeelist/index.tsx
@@ -1,9 +1,100 @@
 import { CoffeeCard } from './coffeeCard'
-import Havaiano from '../../assets/havaiano.svg'
+import Havaiano from '../../assets/coffeeCup/havaiano.svg'
+import Expresso from '../../assets/coffeeCup/expresso.svg'
+import ExpressoAmer from '../../assets/coffeeCup/expressoamer.svg'
+import ExpressoCream from '../../assets/coffeeCup/expressocream.svg'
+import ExpressoGel from '../../assets/coffeeCup/expressogel.svg'
+import CafeComLeite from '../../assets/coffeeCup/comleite.svg'
+import Latte from '../../assets/coffeeCup/latte.svg'
+import Capuccino from '../../assets/coffeeCup/capuccino.svg'
+import Macchiato from '../../assets/coffeeCup/macchiato.svg'
+import Mocaccino from '../../assets/coffeeCup/mocaccino.svg'
+import Choco from '../../assets/coffeeCup/chocoquente.svg'
+import Cubano from '../../assets/coffeeCup/cubano.svg'
+import Arabe from '../../assets/coffeeCup/arabe.svg'
+import Irlandes from '../../assets/coffeeCup/irlandes.svg'
+import { StyledCoffeeList } from './styles'
 
 export function Coffeelist() {
   return (
-    <div>
+    <StyledCoffeeList>
+      <CoffeeCard
+        coffeeCardDescription="Bebida adocicada preparada com café e leite de coco"
+        coffeeCardImg={Expresso}
+        coffeeCardName="Expresso"
+        coffeeCardPrice={9.99}
+        key={2}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={ExpressoAmer}
+        coffeeCardName="Expresso Americano"
+        coffeeCardPrice={9.99}
+        key={3}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={ExpressoCream}
+        coffeeCardName="Expresso Cremoso"
+        coffeeCardPrice={9.99}
+        key={4}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={ExpressoGel}
+        coffeeCardName="Expresso Gelado"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={CafeComLeite}
+        coffeeCardName="Café com Leite"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Latte}
+        coffeeCardName="Latte"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Capuccino}
+        coffeeCardName="Capuccino"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Macchiato}
+        coffeeCardName="Macchiato"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Mocaccino}
+        coffeeCardName="Mocaccino"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Choco}
+        coffeeCardName="Chocolate Quente"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
+      <CoffeeCard
+        coffeeCardDescription="muito bom"
+        coffeeCardImg={Cubano}
+        coffeeCardName="Cubano"
+        coffeeCardPrice={9.99}
+        key={5}
+      />
       <CoffeeCard
         coffeeCardDescription="Bebida adocicada preparada com café e leite de coco"
         coffeeCardImg={Havaiano}
@@ -12,33 +103,19 @@ export function Coffeelist() {
         key={1}
       />
       <CoffeeCard
-        coffeeCardDescription="Bebida adocicada preparada com café e leite de coco"
-        coffeeCardImg="caf"
-        coffeeCardName="Expresso"
-        coffeeCardPrice={9.99}
-        key={2}
-      />
-      <CoffeeCard
         coffeeCardDescription="muito bom"
-        coffeeCardImg="caf"
-        coffeeCardName="expresso"
-        coffeeCardPrice={9.99}
-        key={3}
-      />
-      <CoffeeCard
-        coffeeCardDescription="muito bom"
-        coffeeCardImg="caf"
-        coffeeCardName="expresso"
-        coffeeCardPrice={9.99}
-        key={4}
-      />
-      <CoffeeCard
-        coffeeCardDescription="muito bom"
-        coffeeCardImg="caf"
-        coffeeCardName="expresso"
+        coffeeCardImg={Arabe}
+        coffeeCardName="Arabe"
         coffeeCardPrice={9.99}
         key={5}
       />
-    </div>
+      <CoffeeCard
+        coffeeCardDescription="Bebida adocicada preparada com café e leite de coco"
+        coffeeCardImg={Irlandes}
+        coffeeCardName="Irlandês"
+        coffeeCardPrice={9.99}
+        key={1}
+      />
+    </StyledCoffeeList>
   )
 }
---
+++ src/components/coffeelist/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledCoffeeList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 10rem;
+  gap: 2rem 2.5rem;
+`
