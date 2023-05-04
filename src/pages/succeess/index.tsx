83433e139fcb04ee831ed784e5122dcd81161d64
IURI <123806396+ithauront@users.noreply.github.com>
2023-05-04 19:45:04 +0200

    success structure
    strated the structure for the success page

--- src/pages/succeess/index.tsx
+++ src/pages/succeess/index.tsx
@@ -1,3 +1,42 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import Motoboy from '../../assets/motoboy.svg'
+
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <>
+      <h1>Uhu! Pedido confirmado</h1>
+      <p>Agora é so aguardar que logo o café chegara até você</p>
+      <div className="deliveryRecap">
+        <div>
+          <span>
+            <MapPin />
+          </span>
+          <span>
+            `entrega em
+            <strong>Rua form.rua, form.numero</strong>
+            form.bairro - Form.cidade, Form.UF
+          </span>
+        </div>
+        <div>
+          <span>
+            <Timer />
+          </span>
+          <p>Previsão de entrega</p>
+          <p>20 min - 30 min</p>
+        </div>
+        <div>
+          <span>
+            <CurrencyDollar />
+          </span>
+          <span>
+            <p>Pagamento na entrega</p>
+            <p> Form.paymentButto</p>
+          </span>
+        </div>
+      </div>
+      <div className="successImg">
+        <img src={Motoboy} alt="DeliveryBoy on a moto" />
+      </div>
+    </>
+  )
 }
