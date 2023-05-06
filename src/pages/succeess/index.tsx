f049c1e7ac80ead9c895b2df509987942ea9ecea
IURI <123806396+ithauront@users.noreply.github.com>
2023-05-06 14:06:49 +0200

    success styling done
    all the stylling of the app that is not related to functionality is done.
    now is time to start the fucntionality of the app

--- src/pages/succeess/index.tsx
+++ src/pages/succeess/index.tsx
@@ -1,42 +1,58 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import Motoboy from '../../assets/motoboy.svg'
+import { IconStyle, StyledSuccess } from './styles'
 
 export function Success() {
   return (
-    <>
-      <h1>Uhu! Pedido confirmado</h1>
-      <p>Agora é so aguardar que logo o café chegara até você</p>
-      <div className="deliveryRecap">
-        <div>
-          <span>
-            <MapPin />
-          </span>
-          <span>
-            `entrega em
-            <strong>Rua form.rua, form.numero</strong>
-            form.bairro - Form.cidade, Form.UF
-          </span>
+    <StyledSuccess>
+      <div className="successTitle">
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é so aguardar que logo o café chegara até você</p>
+      </div>
+      <div className="recapImgContainer">
+        <div className="deliveryRecapBorder">
+          <div className="deliveryRecap">
+            <div className="deliveryRecapInfo">
+              <IconStyle className="mapPin">
+                <MapPin />
+              </IconStyle>
+              <p>
+                Entrega em
+                <strong>Rua form.rua, form.numero</strong>
+                <br />
+                form.bairro - Form.cidade, Form.UF
+              </p>
+            </div>
+            <div className="deliveryRecapInfo">
+              <IconStyle className="timer">
+                <Timer />
+              </IconStyle>
+              <p>
+                Previsão de entrega
+                <strong>
+                  <br />
+                  20 min - 30 min
+                </strong>
+              </p>
+            </div>
+            <div className="deliveryRecapInfo">
+              <IconStyle className="dollar">
+                <CurrencyDollar />
+              </IconStyle>
+              <span>
+                <p>
+                  Pagamento na entrega
+                  <br />
+                  <strong>Form.paymentButto</strong>
+                </p>
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <span>
-            <Timer />
-          </span>
-          <p>Previsão de entrega</p>
-          <p>20 min - 30 min</p>
-        </div>
-        <div>
-          <span>
-            <CurrencyDollar />
-          </span>
-          <span>
-            <p>Pagamento na entrega</p>
-            <p> Form.paymentButto</p>
-          </span>
+        <div className="successImg">
+          <img src={Motoboy} alt="DeliveryBoy on a moto" />
         </div>
       </div>
-      <div className="successImg">
-        <img src={Motoboy} alt="DeliveryBoy on a moto" />
-      </div>
-    </>
+    </StyledSuccess>
   )
 }
