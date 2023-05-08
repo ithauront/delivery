34b64e13f23b4669f88a64ab5c893602efad4e69
IURI <123806396+ithauront@users.noreply.github.com>
2023-05-08 22:50:40 +0200

    took upercases out
    took all uppercases of the filters in the coffeeList this way we should have a padronization and avoid errors.
    also now that we dont have anymore uppercases we can take out the functions toLowerCase() that was used two times in shouldRenderCoffeeCard

--- src/components/coffeelist/index.tsx
+++ src/components/coffeelist/index.tsx
@@ -19,11 +19,11 @@ import { Filter } from '../filter'
 
 export function Coffeelist() {
   const [filterStates, setFilterStates] = useState<{ [key: string]: boolean }>({
-    Tradicional: false,
-    Especial: false,
-    'Com Leite': false,
-    Alcoolico: false,
-    Gelado: false,
+    tradicional: false,
+    especial: false,
+    'com leite': false,
+    alcoolico: false,
+    gelado: false,
   })
 
   const handleIsSelected = (name: string) => {
@@ -34,16 +34,16 @@ export function Coffeelist() {
   }
 
   const shouldRenderCoffeeCard = (filterNames: string[]) => {
-    const activeFilters = Object.keys(filterStates)
-      .filter((key) => filterStates[key])
-      .map((filter) => filter.toLowerCase())
+    const activeFilters = Object.keys(filterStates).filter(
+      (key) => filterStates[key],
+    )
 
     if (activeFilters.length === 0) {
       return true
     }
 
     const matchingFilters = filterNames.filter((name) =>
-      activeFilters.includes(name.toLowerCase()),
+      activeFilters.includes(name),
     )
 
     return matchingFilters.length > 0
@@ -55,29 +55,29 @@ export function Coffeelist() {
         <h1>Nossos cafés</h1>
         <section>
           <Filter
-            filterNames={['Tradicional']}
-            isSelected={filterStates.Tradicional}
-            onClick={() => handleIsSelected('Tradicional')}
+            filterNames={['tradicional']}
+            isSelected={filterStates.tradicional}
+            onClick={() => handleIsSelected('tradicional')}
           />
           <Filter
-            filterNames={['Especial']}
-            isSelected={filterStates.Especial}
-            onClick={() => handleIsSelected('Especial')}
+            filterNames={['especial']}
+            isSelected={filterStates.especial}
+            onClick={() => handleIsSelected('especial')}
           />
           <Filter
-            filterNames={['Com Leite']}
-            isSelected={filterStates['Com Leite']}
-            onClick={() => handleIsSelected('Com Leite')}
+            filterNames={['com leite']}
+            isSelected={filterStates['com leite']}
+            onClick={() => handleIsSelected('com leite')}
           />
           <Filter
-            filterNames={['Alcoolico']}
-            isSelected={filterStates.Alcoolico}
-            onClick={() => handleIsSelected('Alcoolico')}
+            filterNames={['alcoolico']}
+            isSelected={filterStates.alcoolico}
+            onClick={() => handleIsSelected('alcoolico')}
           />
           <Filter
-            filterNames={['Gelado']}
-            isSelected={filterStates.Gelado}
-            onClick={() => handleIsSelected('Gelado')}
+            filterNames={['gelado']}
+            isSelected={filterStates.gelado}
+            onClick={() => handleIsSelected('gelado')}
           />
         </section>
       </StyledCoffeeListHeader>
