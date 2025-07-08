0043ffecff2f9c1458b2fb6f8258abbb399f4b9a
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-07-08 13:45:44 +0200

    feat[root/app]: basename in browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
       <CoffeeCardContextProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/delivery">
           <Router />
         </BrowserRouter>
         <GlobalStyle />
